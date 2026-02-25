df53378a64eaa51e35473967e2e499c78adf5075
0-LG-0 <luigigjurashaj@gmail.com>
2026-02-24 21:59:30 -0500

    Setup

---
+++ models/planet.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const planetSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: String,
+  color: { type: String, required: true },
+  isHabitable: Boolean,
+})
+
+const Planet = mongoose.model('Planet', planetSchema)
+module.exports = Planet
---
+++ server.js
@@ -0,0 +1,4 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Planet = require('./models/planet.js')
